006e17ff57b1ae813dff365db025da403b4d720a
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-04-19 15:49:16 -0500

    sasa

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '@styles/globals.css'
+// import '@styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'; // Add this line
+import '../styles/globals.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default Application
+
+
